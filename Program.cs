b6409768266a95abf3685e05b9d1e2428611095c
ronngo <ronngomn23@gmail.com>
2020-09-29 20:50:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LopPointVaMoveablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Point");
+            Point point = new Point(5.6f, 7.9f);
+
+
+            Console.WriteLine(point);
+            Console.WriteLine("MovablePoint");
+            MovablePoint movablePoint = new MovablePoint(8.2f, 20.6f);
+            Console.WriteLine(movablePoint.Move());
+            Console.WriteLine(movablePoint.ToString());
+        }
+    }
+}
